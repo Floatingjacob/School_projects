eefb662e857a4c070de79317752917e249f7e95d
Jacob <108031526+Floatingjacob@users.noreply.github.com>
2024-08-22 18:27:01 -0500

    Add files via upload

---
+++ Hello-World/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World, My name is Jacob Miller!")
+        Console.WriteLine("Press any key to continue...")
+        Console.ReadKey()
+    End Sub
+End Module
