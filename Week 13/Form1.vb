3e48410df717404e2edb6252eb550e4b7413b6d7
Jacob <108031526+Floatingjacob@users.noreply.github.com>
2024-11-15 20:00:54 -0600

    Add files via upload

---
+++ Week 13/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private datatable As New DataTable("YRC")
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        datatable.TableName = "YRC_Table"
+        datatable.Columns.Add("YRC_Column")
+        datatable.Rows.Add(TextBox1.Text)
+        datatable.WriteXml("YourRandomCrap.xml")
+        Using dataset As New DataSet
+            dataset.ReadXml("YourRandomCrap.xml")
+            datatable = dataset.Tables(0)
+        End Using
+    End Sub
+End Class
