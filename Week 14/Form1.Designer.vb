ba5dd99c38e119d22e2bfcb137b5291531b50a2a
Jacob <108031526+Floatingjacob@users.noreply.github.com>
2024-11-22 11:30:51 -0600

    Add files via upload

---
+++ Week 14/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        SaveFileDialog1 = New SaveFileDialog()
+        MenuStrip1 = New MenuStrip()
+        FIleToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem1 = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem1 = New ToolStripMenuItem()
+        OpenFileDialog1 = New OpenFileDialog()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(0, 24)
+        TextBox1.MaxLength = 0
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(800, 424)
+        TextBox1.TabIndex = 0
+        ' 
+        ' SaveFileDialog1
+        ' 
+        SaveFileDialog1.DefaultExt = "txt"
+        SaveFileDialog1.Filter = "Text files|*.txt|All Files| *.*"
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.BackColor = Color.Gray
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FIleToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 2
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FIleToolStripMenuItem
+        ' 
+        FIleToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {OpenToolStripMenuItem1, NewToolStripMenuItem, SaveToolStripMenuItem, ExitToolStripMenuItem1})
+        FIleToolStripMenuItem.Name = "FIleToolStripMenuItem"
+        FIleToolStripMenuItem.Size = New Size(37, 20)
+        FIleToolStripMenuItem.Text = "File"
+        ' 
+        ' OpenToolStripMenuItem1
+        ' 
+        OpenToolStripMenuItem1.Name = "OpenToolStripMenuItem1"
+        OpenToolStripMenuItem1.Size = New Size(103, 22)
+        OpenToolStripMenuItem1.Text = "Open"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(103, 22)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.Size = New Size(103, 22)
+        SaveToolStripMenuItem.Text = "Save"
+        ' 
+        ' ExitToolStripMenuItem1
+        ' 
+        ExitToolStripMenuItem1.Name = "ExitToolStripMenuItem1"
+        ExitToolStripMenuItem1.Size = New Size(103, 22)
+        ExitToolStripMenuItem1.Text = "Exit"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        OpenFileDialog1.Filter = "Text files|*.txt|All Files| *.*"
+        OpenFileDialog1.ShowPreview = True
+        OpenFileDialog1.ShowReadOnly = True
+        OpenFileDialog1.SupportMultiDottedExtensions = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FIleToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+
+End Class
---
+++ Week 14/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Try
+            SaveFileDialog1.ShowDialog()
+            File.WriteAllTextAsync(SaveFileDialog1.FileName, TextBox1.Text)
+        Finally
+        End Try
+    End Sub
+
+    Private Sub OpenToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem1.Click
+        Try
+            If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+                TextBox1.Text = File.ReadAllText(OpenFileDialog1.FileName)
+            End If
+        Finally
+        End Try
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Try
+            If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+                File.WriteAllTextAsync(OpenFileDialog1.FileName, TextBox1.Text)
+            End If
+        Finally
+        End Try
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Try
+            TextBox1.Text = ""
+        Finally
+        End Try
+    End Sub
+
+    Private Sub ExitToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem1.Click
+        Close()
+    End Sub
+
+End Class
