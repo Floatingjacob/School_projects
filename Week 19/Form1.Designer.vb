c682522438469968e3981e6ac5e7735d62e31757
Jacob <108031526+Floatingjacob@users.noreply.github.com>
2025-01-16 17:35:17 -0600

    Add files via upload

---
+++ Week 19/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        RichTextBox1 = New RichTextBox()
+        Label1 = New Label()
+        RichTextBox2 = New RichTextBox()
+        RichTextBox3 = New RichTextBox()
+        RichTextBox4 = New RichTextBox()
+        RichTextBox5 = New RichTextBox()
+        RichTextBox6 = New RichTextBox()
+        RichTextBox7 = New RichTextBox()
+        RichTextBox8 = New RichTextBox()
+        RichTextBox9 = New RichTextBox()
+        RichTextBox10 = New RichTextBox()
+        RichTextBox11 = New RichTextBox()
+        Label2 = New Label()
+        RichTextBox12 = New RichTextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(186, 122)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(318, 182)
+        RichTextBox1.TabIndex = 0
+        RichTextBox1.Text = ""
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(264, 106)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(120, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "paste a sentence here"
+        ' 
+        ' RichTextBox2
+        ' 
+        RichTextBox2.Location = New Point(34, 22)
+        RichTextBox2.Name = "RichTextBox2"
+        RichTextBox2.Size = New Size(100, 30)
+        RichTextBox2.TabIndex = 2
+        RichTextBox2.Text = ""
+        ' 
+        ' RichTextBox3
+        ' 
+        RichTextBox3.Location = New Point(34, 68)
+        RichTextBox3.Name = "RichTextBox3"
+        RichTextBox3.Size = New Size(100, 30)
+        RichTextBox3.TabIndex = 3
+        RichTextBox3.Text = ""
+        ' 
+        ' RichTextBox4
+        ' 
+        RichTextBox4.Location = New Point(34, 160)
+        RichTextBox4.Name = "RichTextBox4"
+        RichTextBox4.Size = New Size(100, 30)
+        RichTextBox4.TabIndex = 4
+        RichTextBox4.Text = ""
+        ' 
+        ' RichTextBox5
+        ' 
+        RichTextBox5.Location = New Point(34, 112)
+        RichTextBox5.Name = "RichTextBox5"
+        RichTextBox5.Size = New Size(100, 30)
+        RichTextBox5.TabIndex = 5
+        RichTextBox5.Text = ""
+        ' 
+        ' RichTextBox6
+        ' 
+        RichTextBox6.Location = New Point(34, 200)
+        RichTextBox6.Name = "RichTextBox6"
+        RichTextBox6.Size = New Size(100, 30)
+        RichTextBox6.TabIndex = 6
+        RichTextBox6.Text = ""
+        ' 
+        ' RichTextBox7
+        ' 
+        RichTextBox7.Location = New Point(34, 416)
+        RichTextBox7.Name = "RichTextBox7"
+        RichTextBox7.Size = New Size(100, 30)
+        RichTextBox7.TabIndex = 11
+        RichTextBox7.Text = ""
+        ' 
+        ' RichTextBox8
+        ' 
+        RichTextBox8.Location = New Point(34, 328)
+        RichTextBox8.Name = "RichTextBox8"
+        RichTextBox8.Size = New Size(100, 30)
+        RichTextBox8.TabIndex = 10
+        RichTextBox8.Text = ""
+        ' 
+        ' RichTextBox9
+        ' 
+        RichTextBox9.Location = New Point(34, 376)
+        RichTextBox9.Name = "RichTextBox9"
+        RichTextBox9.Size = New Size(100, 30)
+        RichTextBox9.TabIndex = 9
+        RichTextBox9.Text = ""
+        ' 
+        ' RichTextBox10
+        ' 
+        RichTextBox10.Location = New Point(34, 284)
+        RichTextBox10.Name = "RichTextBox10"
+        RichTextBox10.Size = New Size(100, 30)
+        RichTextBox10.TabIndex = 8
+        RichTextBox10.Text = ""
+        ' 
+        ' RichTextBox11
+        ' 
+        RichTextBox11.Location = New Point(34, 238)
+        RichTextBox11.Name = "RichTextBox11"
+        RichTextBox11.Size = New Size(100, 30)
+        RichTextBox11.TabIndex = 7
+        RichTextBox11.Text = ""
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(146, 50)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(202, 15)
+        Label2.TabIndex = 12
+        Label2.Text = "Choose ten words you want to count"
+        ' 
+        ' RichTextBox12
+        ' 
+        RichTextBox12.Location = New Point(556, 34)
+        RichTextBox12.Name = "RichTextBox12"
+        RichTextBox12.Size = New Size(170, 318)
+        RichTextBox12.TabIndex = 13
+        RichTextBox12.Text = ""
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(276, 350)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 14
+        Button1.Text = "search"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(RichTextBox12)
+        Controls.Add(Label2)
+        Controls.Add(RichTextBox7)
+        Controls.Add(RichTextBox8)
+        Controls.Add(RichTextBox9)
+        Controls.Add(RichTextBox10)
+        Controls.Add(RichTextBox11)
+        Controls.Add(RichTextBox6)
+        Controls.Add(RichTextBox5)
+        Controls.Add(RichTextBox4)
+        Controls.Add(RichTextBox3)
+        Controls.Add(RichTextBox2)
+        Controls.Add(Label1)
+        Controls.Add(RichTextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents RichTextBox4 As RichTextBox
+    Friend WithEvents RichTextBox5 As RichTextBox
+    Friend WithEvents RichTextBox6 As RichTextBox
+    Friend WithEvents RichTextBox7 As RichTextBox
+    Friend WithEvents RichTextBox8 As RichTextBox
+    Friend WithEvents RichTextBox9 As RichTextBox
+    Friend WithEvents RichTextBox10 As RichTextBox
+    Friend WithEvents RichTextBox11 As RichTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RichTextBox12 As RichTextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Week 19/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private searchResults As New Dictionary(Of String, Integer)
+
+    Public Sub SearchAndCount(searchText As String, searchLabel As String)
+        Dim npos As Integer = 0
+        Dim count As Integer = 0
+        Do
+            npos = InStr(npos + 1, RichTextBox1.Text, searchText)
+            If npos > 0 Then
+                count += 1
+            End If
+        Loop While npos > 0
+        If Not String.IsNullOrWhiteSpace(searchText) Then
+            searchResults($"{searchLabel}: {searchText}") = count
+        End If
+    End Sub
+
+    Private Sub DisplayResults()
+        RichTextBox12.Clear()
+        Dim sortedResults = searchResults.OrderByDescending(Function(kvp) kvp.Value)
+        For Each result In sortedResults
+            RichTextBox12.AppendText($"{result.Key} = {result.Value}{Environment.NewLine}")
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        searchResults.Clear()
+
+        SearchAndCount(RichTextBox2.Text, "Search 2")
+        SearchAndCount(RichTextBox3.Text, "Search 3")
+        SearchAndCount(RichTextBox4.Text, "Search 4")
+        SearchAndCount(RichTextBox5.Text, "Search 5")
+        SearchAndCount(RichTextBox6.Text, "Search 6")
+        SearchAndCount(RichTextBox7.Text, "Search 7")
+        SearchAndCount(RichTextBox8.Text, "Search 8")
+        SearchAndCount(RichTextBox9.Text, "Search 9")
+        SearchAndCount(RichTextBox10.Text, "Search 10")
+        SearchAndCount(RichTextBox11.Text, "Search 11")
+        DisplayResults()
+    End Sub
+End Class
