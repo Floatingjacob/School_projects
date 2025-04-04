3f114315eb6cdd4ba11e382525e556f259c73575
Jacob <108031526+Floatingjacob@users.noreply.github.com>
2025-04-04 15:48:06 -0500

    Add files via upload

---
+++ Week29/Week29/Week29/Program.cs
@@ -0,0 +1,33 @@
+using MudBlazor.Services;
+using Week29.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Week29.Client._Imports).Assembly);
+
+app.Run();
